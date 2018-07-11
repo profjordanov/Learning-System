86a5bab1d8b07eb4c95cd8c165341d732c7fbe6d
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-11 10:38:37 +0300

    FormFileExtensions - ToByteArrayAsync

---
+++ LearningSystem.Web/Infrastructure/Extensions/FormFileExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace LearningSystem.Web.Infrastructure.Extensions
+{
+    public static class FormFileExtensions
+    {
+        public static async Task<byte[]> ToByteArrayAsync(this IFormFile formFile)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                await formFile.CopyToAsync(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
