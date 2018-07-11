ea8dc2b4ac195fea2e2d2968ddf34a12307febdd
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-11 10:34:02 +0300

    ControllerExtensions Added

---
+++ LearningSystem.Web/Infrastructure/Extensions/ControllerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningSystem.Web.Infrastructure.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static IActionResult ViewOrNotFound(this Controller controller, object model)
+        {
+            if (model == null)
+            {
+                return controller.NotFound();
+            }
+
+            return controller.View(model);
+        }
+    }
+}
