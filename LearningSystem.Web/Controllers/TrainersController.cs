5736f6d747c71d78951c9b1e9388eeb93e16fa4e
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-12 12:23:54 +0300

    TrainersController

---
+++ LearningSystem.Web/Controllers/TrainersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningSystem.Web.Controllers
+{
+    public class TrainersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
