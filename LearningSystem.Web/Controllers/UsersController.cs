b5a3afe6ebec3f867aa092fc7567bc733244870b
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-12 12:24:51 +0300

    UsersController

---
+++ LearningSystem.Web/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningSystem.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
